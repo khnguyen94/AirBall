bbcdce0d0015939001b0bb541627b631b3fe836f
Khoa Nguyen <kh.nguyen0101@gmail.com>
2020-02-05 18:36:18 -0800

    #'s to hold place for slider images for homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -7,12 +7,12 @@ class Home extends Component {
   render() {
     const images = [
       { url: "images/1.jpg" },
-      { url: "images/2.jpg" },
-      { url: "images/3.jpg" },
-      { url: "images/4.jpg" },
-      { url: "images/5.jpg" },
-      { url: "images/6.jpg" },
-      { url: "images/7.jpg" }
+      { url: "#" },
+      { url: "#" },
+      { url: "#" },
+      { url: "#" },
+      { url: "#" },
+      { url: "#" }
     ];
 
     return (
