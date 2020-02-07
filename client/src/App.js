d20de3739e0afd318c711189f594ed17bc476124
Khoa Nguyen <kh.nguyen0101@gmail.com>
2020-02-07 12:31:51 -0800

    added bootstrap cdn and all bootstrap related plug in dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+<<<<<<< HEAD
 import logo from "./logo.jpg";
+=======
+import logo from "./Logo/Air_Ball_Logo.jpg";
+>>>>>>> 60863ab7244d73e356fdf9352bb424cd8922c432
 import Events from "./pages/Events";
 import Nav from "./components/Nav";
 import "./App.css";
