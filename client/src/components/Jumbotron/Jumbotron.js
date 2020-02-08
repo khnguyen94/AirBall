b3fb1896707be2261a16bbd3a4bc0775823da0ad
Khoa Nguyen <kh.nguyen0101@gmail.com>
2020-02-08 03:43:03 -0800

    differentiated between favorite btns for teams and game events, created a new one for game events

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,20 +1,11 @@
 import React from "react";
 
-function Jumbotron({ children }) {
+function Jumbotron({props}) {
   return (
-<<<<<<< HEAD
     <div
       className="jumbotron"
     >
-      {children}
-=======
-    <div className="jumbotron jumbotron-fluid">
-      <div className="container">
-        
-        {children}
-        
-      </div>
->>>>>>> 011b6cec09274b7b1fb0465843c59af0f2f92d84
+      {props}
     </div>
   );
 }
