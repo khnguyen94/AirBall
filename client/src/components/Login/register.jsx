a65bb68f0953b7e772d47113bb50449c5bcec588
Kneeslapper <gloriaklee8@gmail.com>
2020-02-07 20:48:22 -0800

    added first/last name and email

--- client/src/components/Login/register.jsx
+++ client/src/components/Login/register.jsx
@@ -14,26 +14,32 @@ export class Register extends React.Component {
                 <div className="image">
 
                 </div>
-                <div className="form">
-                    <div className="form-group">
-                        <label htmlFor="username">UserName</label>
-                        <input type="text" name="username" placeholder="username" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email" placeholder="email" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" placeholder="password" />
-                    </div>
+                <div className="form-group">
+                    <label htmlFor="firstname">First Name</label>
+                    <input type="text" name="firstname" placeholder="first name" />
                 </div>
-            </div>
+                <div className="form-group">
+                    <label htmlFor="lastname">Last Name</label>
+                    <input type="text" name="username" placeholder="last name" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" placeholder="email" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="username">UserName</label>
+                    <input type="text" name="username" placeholder="username" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" placeholder="password" />
+                </div>
+            </div >
             <div className="footer">
                 <button type="button" className="btn">
                     Register
                 </button>
             </div>
-        </div>
+        </div >
     }
 }
