5ef3c649f3c37aafa48f0f874ea6eeb55a2ac500
Khoa Anh Nguyen <kh.nguyen0101@gmail.com>
2020-02-15 14:10:23 -0800

    Components (#36)
    * formatted rendered team list
    
    * added a function that handles on favorite click and API update isFavorite/checked submission for a rendered team's favorite button
    
    * added a function that handles onclick for favorite button of an already favorited team, send the checked state to API and rerenders the favorite teams list
    
    * fixed .then render method to renderFaveTeams
    
    * created an extra condition on initial render function's filters that additionally checks for the nbaFranchise prop of each object to be 1 which is the USA professional teams only
    
    * formatted sidebar and pulled API update calls out

--- client/src/components/FavoriteTeamBtn/FavoriteTeamBtn.js
+++ client/src/components/FavoriteTeamBtn/FavoriteTeamBtn.js
@@ -1,31 +1,36 @@
 import React, { Component } from "react";
 import Switch from "react-switch";
- 
+import "./FavoriteTeamBtn.css";
+import API from "../../utils/API";
+
 class FavoriteTeamBtn extends Component {
-  constructor() {
-    super();
-    this.state = { favorite: false };
-    this.handleChange = this.handleChange.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = { checked: false };
+    this.onClick = this.onClick.bind(this);
   }
 
-  handleChange(checked) {
-    this.setState({ checked });
+  // When the favorite button is clicked, use the API.updateTeamFavorite method to save the newly favorited team's isFavorite property
+  // checked is now set to true
+  // Then reload favoriteTeams list in sideBar
+  onClick(event) {
+    event.preventDefault();
+    this.setState({ checked: true });
   }
 
   render() {
     return (
-      <div className="favoriteTeamToggle">
-        <label>
-          <Switch
+      <label className="favoriteTeamToggle">
+        <Switch
           height={15}
-            onChange={this.handleChange}
-            favorite={this.state.favorite}
-            className="react-switch"
-          />
-        </label>
-      </div>
+          width={30}
+          onClick={this.onClick}
+          checked={this.state.checked}
+          className="react-switch"
+        />
+      </label>
     );
   }
 }
 
-export default FavoriteTeamBtn; 
+export default FavoriteTeamBtn;
